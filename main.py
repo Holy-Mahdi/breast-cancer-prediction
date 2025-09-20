8bccacb4229f6f9d5d251439514b0899b5c9a808
Mahdi Mohammadi <MohammadiMahdiOfficial@gmail.com>
2025-09-20 16:20:45 +0330

    complete Logistic Regression

--- main.py
+++ main.py
@@ -14,3 +14,15 @@ y = data.target
 
 feature_name = data.feature_names
 target_name = data.target_names
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scalar = StandardScaler()
+X_train = scalar.fit_transform(X_train)
+X_test = scalar.transform(X_test)
+
+
+lr_model = LogisticRegression(max_iter=10000)
+lr_model.fit(X_train, y_train)
+y_pred_lr = lr_model.predict(X_test)
